5a846cee4382c70d4d9156ede0e3d2b6e180a0f3
BerciBAA <bercike222@gmail.com>
2023-06-26 19:26:33 +0200

    Eureka server link change

--- micro-services/config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ micro-services/config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
-w
+
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
